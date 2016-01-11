03c3df4bf41e496d7e2b5e14c21eb242aa5d0408
hector <giovanni.rodriguez.ramos@gmail.com>
2016-01-11 12:20:30 -0600

    GiovanniPC: Lunes 11 de Enero de 2016

---
+++ application/modules/encuesta/controllers/IndexController.php
@@ -0,0 +1,46 @@
+<?php
+
+class Encuesta_IndexController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function adminAction()
+    {
+        // action body
+    }
+
+    public function altaAction()
+    {
+        // action body
+    }
+
+    public function editaAction()
+    {
+        // action body
+    }
+
+    public function bajaAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
---
+++ library/Util/Plugin/CheckAccess.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * 
+ */
+class Zazil_Plugin_CheckAccess extends Zend_Controller_Plugin_Abstract {
+	
+	private $_auth;
+	private $_acl;
+	
+	protected $_module;
+	protected $_controller;
+	protected $_action;
+	protected $_currentRole;
+	protected $_resource;
+	
+	static private $instance = NULL;
+	
+	public function __construct() {
+		$this->_auth = Zend_Auth::getInstance();
+		$this->_acl = new Zazil_Permissions_Acl(APPLICATION_PATH . '/configs/permissions.ini');
+	}
+	
+	protected function _init(Zend_Controller_Request_Abstract $request) {
+		$this->_module = $request->getModuleName();
+		$this->_controller = $request->getControllerName();
+		$this->_action = $request->getActionName();
+		$this->_resource = $this->_module . ':' . $this->_controller;
+	}
+	
+	public static function getInstance() {
+		if (is_null(self::$instance)) {
+			self::$instance = new Zazil_Plugin_CheckAccess();
+		}
+		return self::$instance;
+	}
+	
+	/**
+     * Retorna el Rol del usuario actual
+     * 
+     * @return string
+     */
+    private function getRol()
+    {
+    	$rolStr = 'guest';
+    	if($this->_auth->hasIdentity()){
+    		$numRol = $this->_auth->getIdentity()->rol;
+			$tablaRol = new Zend_Db_Table('roles');
+			$rol = $tablaRol->fetchRow($tablaRol->select()->from($tablaRol)->where('idRol = ?', $numRol));
+			$rolStr = $rol['rol'];
+    	}
+		
+        return $rolStr;
+    }
+	
+	public function preDispatch(Zend_Controller_Request_Abstract $request) {
+		$controlleName = $request->getControllerName();
+		if ($this->_auth->hasIdentity()) {
+			
+		}else{
+			if($controlleName != 'login'){
+				$request->setControllerName('login');
+				$request->setActionName('index');
+			}
+		}
+	}
+	
+	/**
+     * isAllowed
+     * 
+     * Retorna si tiene los permisos necesarios para el recurso y el permiso
+     * solicitado
+     * 
+     * @param  string  $resource
+     * @param  string  $permission optional
+     * @return bool
+     */
+    public function isAllowed ($resource, $permission = null) {
+        // Por defecto, no tiene permisos
+        $allow = false;
+ 
+        // Si solo pregunta por el recurso
+        if (is_null($permission)) {
+            $allow = $this->_acl->isAllowed($this->getRol(), $resource);
+        }
+        // Si pregunta por el recurso y el permiso
+        else {
+            $allow = $this->_acl->isAllowed($this->getRol(), $resource, $permission);
+        }
+ 
+        return $allow;
+    }
+}
