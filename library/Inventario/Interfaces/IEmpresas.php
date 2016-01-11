389e1c4f07f96ebb0e1d66756cc2dd150a918c4a
hector <giovanni.rodriguez.ramos@gmail.com>
2016-01-10 22:13:43 -0600

    GiovanniLap: Domingo 10 de Enero de 2016

--- library/Inventario/Interfaces/IEmpresas.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-interface Inventario_Interfaces_IFiscales {
-	public function obtenerFiscales($idFiscales);
-	public function obtenerDomicilios($idFiscales);
-	public function obtenerTelefonos($idFiscales);
-	public function obtenerEmails($idFiscales);
-}
