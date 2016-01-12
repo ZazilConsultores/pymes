a87f58171f5937659f749d4518e9a85e595a3b76
areli <areli.morales@zazil.net>
2016-01-12 12:12:14 -0600

    Areli fecha:12 de Enero 2016 x2

--- library/Inventario/Interfaces/IEmpresa.php
+++ library/Inventario/Interfaces/IEmpresa.php
@@ -2,8 +2,6 @@
 
 interface Inventario_Interfaces_IEmpresa {
 	public function obtenerEmpresas();
-	//Areli
-	public function obtenerempresa();
 	public function obtenerClientes();
 	public function obtenerProveedores();
 }
