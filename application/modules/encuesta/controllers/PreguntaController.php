08d251785d32838bc2efbe93549f7a2a4a626257
hector <giovanni.rodriguez.ramos@gmail.com>
2016-02-04 10:08:50 -0600

    GiovanniPC: Jueves 4 de Febrero de 2016

--- application/modules/encuesta/controllers/PreguntaController.php
+++ application/modules/encuesta/controllers/PreguntaController.php
@@ -88,9 +88,12 @@ class Encuesta_PreguntaController extends Zend_Controller_Action
 				$pregunta = $this->preguntaDAO->crearPregunta($datos["idOrigen"], $datos["origen"], $pregunta);
 				
 				if($pregunta->getTipo() == "AB"){
-					//Navegamos al alta de opciones de seleccion
-					$this->_helper->redirector->gotoSimple("index", "seccion", "encuesta", array("idSeccion" => $pregunta->getIdOrigen()));
-					
+					if($datos["origen"] == "S"){
+						$this->_helper->redirector->gotoSimple("index", "seccion", "encuesta", array("idSeccion" => $pregunta->getIdOrigen()));
+					}elseif($datos["origen"] == "G"){
+						$this->_helper->redirector->gotoSimple("index", "grupo", "encuesta", array("idGrupo" => $pregunta->getIdOrigen()));
+						//$this->_helper->redirector->gotoSimple("index", "grupo", "encuesta", array("idGrupo" => $pregunta->getIdOrigen()));
+					}
 				}else{
 					if($datos["origen"] == "S"){
 						$this->_helper->redirector->gotoSimple("opciones", "pregunta", "encuesta", array("idPregunta" => $pregunta->getIdPregunta()));
