cbc9c24d72adee2801fdc543c9fb700fc5dd0a26
Hector Giovanni Rodriguez Ramos <giovanni.rodriguez@zazil.net>
2016-05-24 16:44:52 -0500

    GiovanniLap: Martes 24 de Mayo de 2016

--- public/index.php
+++ public/index.php
@@ -81,7 +81,7 @@ Zend_Registry::set('connlocal', $connlocal);
 $db = Zend_Db::factory('PDO_MYSQL', $connlocal);
 //$db = Zend_Db::factory('PDO_MYSQL', $connnancy);
 //$db = Zend_Db::factory('PDO_MYSQL', $connlocaldos);
-//$db = Zend_Db::factory('PDO_MYSQL', $connserver);
+$db = Zend_Db::factory('PDO_MYSQL', $connserver);
 
 $db->query("SET NAMES 'utf8'");
 $db->query("SET CHARACTER SET 'utf8'");
