621c384806d0136512245a9cca229a31620b2571
areli <areli.morales@zazil.net>
2016-07-01 20:08:05 -0500

    AreliLap 01 de Julio  2016

--- application/modules/inventario/controllers/InventarioController.php
+++ application/modules/inventario/controllers/InventarioController.php
@@ -2,10 +2,14 @@
 
 class Inventario_InventarioController extends Zend_Controller_Action
 {
-	private $inventarioDAO;
-    public function init()
+	private $inventarioDAO = null;
+
+	private $productoDAO = null;
+	
+	public function init()
     {
-    	$this->inventarioDAO = new Inventario_DAO_Inventario;
+    	$this->inventarioDAO = new Inventario_DAO_Inventario;  
+		$this->productoDAO = new Inventario_DAO_Producto;
     }
 
     public function indexAction()
@@ -17,12 +21,39 @@ class Inventario_InventarioController extends Zend_Controller_Action
 
     public function adminAction()
     {
-        // action body
+        $idInventario = $this -> getParam("idInventario");
+	
+	
+		$inventario = $this->inventarioDAO->obtenerProductoInventario($idInventario);
+		$formulario = new Inventario_Form_AdminInventario;
+		
+		
+		$formulario->getElement("minimo")->setValue($inventario->getMinimo());
+		$formulario->getElement("maximo")->setValue($inventario->getMaximo());
+		$formulario->getElement("costoUnitario")->setValue($inventario->getCostoUnitario());
+		$formulario->getElement("porcentajeGanancia")->setValue($inventario->getPorcentajeGanancia());
+		$formulario->getElement("cantidadGanancia")->setValue($inventario->getCantidadGanancia());
+		$formulario->getElement("costoCliente")->setValue($inventario->getCostoCliente());
+		$formulario->getElement("submit")->setLabel("Actualizar Producto");
+		$formulario->getElement("submit")->setAttrib("class", "btn btn-warning");
+		
+		
+		$this -> view -> inventario = $inventario;
+		$this -> view -> formulario = $formulario;
     }
 
     public function editaAction()
     {
         // action body
+        $idInventario = $this->getParam("idInventario");
+		
+		$datos = $this->getRequest()->getPost();
+		unset($datos["submit"]);
+			
+		$this->inventarioDAO->editarInventario($idInventario, $datos);
+
+		
+		$this->_helper->redirector->gotoSimple("admin", "inventario", "inventario", array("idInventario"=>$idInventario));
     }
 }
 
--- library/Inventario/DAO/Inventario.php
+++ library/Inventario/DAO/Inventario.php
@@ -7,33 +7,104 @@
 class Inventario_DAO_Inventario implements Inventario_Interfaces_IInventario {
 	
 	private $tablaInventario;
+	private $tablaProducto;
 	
 	public function __construct() {
-		$this->tablaInventario = new  Inventario_Model_Inventario;
+		$this->tablaInventario = new Inventario_Model_DbTable_Inventario;
+		$this->tablaProducto = new Inventario_Model_DbTable_Producto;
 	}
 	
 	public function obtenerInventario(){
 		$tablaInventario = $this->tablaInventario;
-	
-		$rowInventario = $tablaInventario>fetchAll();
+		$rowsInventario = $tablaInventario->fetchAll();
 		
-		$modelInventarios = array();
+		$modelInventario = array();
 		
-		foreach ($rowInventarios as $rowInventario) {
+		foreach($rowsInventario as $rowInventario){
 			$modelInventario = new Inventario_Model_Inventario($rowInventario->toArray());
-			$modelInventario>setIdInventario($rowInventario->idInventario);
+			$modelInventario->setIdInventario($rowInventario->idInventario);
 			
-			$modelInventarios[] = $modelInventarios;
+			$modelsInventario [] = $modelInventario;
+			
 		}
 		
-		return $modelInventarios;
+		return $modelsInventario;
 	}
 	
-	public function editarInventario($idInventario, Sistema_Model_Inventario $inventario){
-		$tablaInventario = $this->tablaEmail;
-		$where = $tablaEmail->getAdapter()->quoteInto("idEmail = ?", $idEmail);
+public function obtenerIdProductoInventario(){ 
+		$tablaProducto = $this->tablaProducto;
+		$tablaInventario = $this->tablaInventario;
 		
-		$tablaEmail->update($email->toArray(), $where);
+		$rowsInventario = $tablaInventario->fetchAll();
+		//$inventario = $rowsInventario;
+		
+		$idProducto = array();
+		
+		foreach ($rowsInventario as $row){
+			$select = $tablaProducto->select()->from($tablaProducto)->where("idProducto = ?",$row->idProducto);
+			$rowProducto= $tablaProducto->fetchRow($select);
+			
+			$idProducto[] = $rowProducto->idProducto;
+		}
+		return $idProducto;
+	}
+	
+	public function obtenerProductoInventario($idInventario){
+		$tablaInventario = $this->tablaInventario;
+		$select = $tablaInventario->select()->from($tablaInventario)->where("idInventario = ?", $idInventario);
+		$rowInventario = $tablaInventario->fetchRow($select);
+		
+		$inventarioModel = new Inventario_Model_Inventario($rowInventario->toArray());
+		$inventarioModel->setIdInventario($rowInventario->idInventario);
+		
+		return $inventarioModel;
+		
+	}
+	
+	public function editarInventario($idInventario, array $inventario)
+	{
+		$tablaInventario = $this->tablaInventario;
+		$where = $tablaInventario->getAdapter()->quoteInto("idInventario = ?", $idInventario);
+		$tablaInventario->update($inventario, $where);
+		
+		//*******Edita
+		$datos=array();
+		$bd = Zend_Db_Table_Abstract::getDefaultAdapter();
+		$bd->beginTransaction();
+		
+		
+		
+		try{
+		$tablaInventario = $this->tablaInventario;
+		$select = $tablaInventario->select()->from($tablaInventario)->where("idInventario=?",$datos['idInventario']);
+	
+		$row = $tablaInventario->fetchRow($select); 
+		
+		if(!is_null($row)){
+			$secuencial= $row->secuencial +1;
+			print_r($secuencial);
+		}else{
+			$secuencial = 1;	
+		
+		print_r($secuencial);
+		}
+			
+		$bd->commit();
+		}catch(exception $ex){
+			print_r("<br />");
+			print_r("================");
+			print_r("<br />");
+			print_r("Excepcion Lanzada");
+			print_r("<br />");
+			print_r("================");
+			print_r("<br />");
+			print_r($ex->getMessage());
+			print_r("<br />");
+			print_r("<br />");
+			$bd->rollBack();
+		}
+	
+	
 	}
 	
 
