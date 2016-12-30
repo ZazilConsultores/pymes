b31722bde4dee85f902b1172ec0bbdc7579f7fe6
Hector Rodriguez <giovanni.rodriguez@zazil.net>
2016-12-29 21:31:52 -0600

    GiovanniLap: Jueves 29 de Diciembre de 2016x3

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <?php
-
 // Define path to application directory
 defined('APPLICATION_PATH')
-    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . './application'));
+    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/application'));
 // Define path of application modules
 defined('MODULES_PATH')
-        || define('MODULES_PATH', realpath(dirname(__FILE__) . './application/modules'));
-
+        || define('MODULES_PATH', realpath(dirname(__FILE__) . '/application/modules'));
 // Define application environment
 defined('APPLICATION_ENV')
     || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'development'));
@@ -28,349 +26,146 @@ require_once 'Zend/Application.php';
 // Create application, bootstrap, and run
 $application = new Zend_Application(
     APPLICATION_ENV,
-    APPLICATION_PATH . './configs/application.ini'
+    APPLICATION_PATH . '/configs/application.ini'
 );
 /**
  * Agregamos variables al registro de Zend,
  * este se mantendra en memoria: no acepta objetos, solo variables y arrays.
  */
 setlocale(LC_ALL, 'es_MX.UTF-8');
-/* 
-setlocale(LC_MONETARY, 'es_MX.UTF-8');
->>>>>>> 2f6fa1e7a2ac4c32b496a12d645eee30967bc69f:public/index.php
-//============================================================================ CONSTANTES GENERALES
-//$estatusEncuesta = array('0' => 'CREADO', '1' => 'PUBLICADO', '2' => 'ACTIVO', '3' => 'FINALIZADO');
-
-$tipo = array('AB' => 'ABIERTAS', 'SS' => 'SIMPLE SELECCION', 'MS' => 'MULTIPLE SELECCION');
-$formaPago = array('CH'=>'CHEQUE','DE'=>'DEPOSITO','DO'=>'DOCUMENTO','EF'=>'EFECTIVO','SP'=>'SPEI','TR'=>'TRANSFERENCIA');
-$padre = array('G' => 'GRUPO', 'S' => 'SECCION');
-$estatus = array('A' => 'ACTIVO', 'C' => 'CANCELADO');
-$tUsuario = array('AL' => 'Alumna', 'DO' => 'Docente', 'MA' => 'Mantenimiento', 'LI' => 'Limpieza', 'SI' => 'Sistemas','AD' => 'Administrativo');
-$tipoEmpresa = array("EM"=>"Empresa","CL"=>"Cliente","PR"=>"Proveedor");
-$tipoBanco = array("CA" => "Caja","IN" => "Inversiones","OP" => "Operacion");
-$tipoTelefono = array("OF"=>"Oficina","CL"=>"Celular");
-$tipoSucursal = array("SE"=>"Sucursal Empresa","SC"=>"Sucursal Cliente", "SP" => "Sucursal Proveedor");
-
-$conceptoPago = array('AN'=>'Anticipo', 'LI'=>'Liquidacion', 'PA'=>'Pago','PE'=>'Pendiente Pago');
-
-$tiposValores = array("EN" => "Entero", "TX" => "Texto", "DC" => "Decimal");
-
-//$tipoEmail = array("OF"=>"Oficina","CS"=>"Casa","PR"=>"Proveedor");
-$tipoMantenimiento = array("MH"=>"Mantenimiento Hardware","MS"=>"Mantenimiento Software","AV"=>"Antivirus","RO"=>"Registro Observaciones");
-$gradosEscolares = array(1=>"1°",2=>"2°",3=>"3°",4=>"4°",5=>"5°",6=>"6°",7=>"7°",8=>"8°",9=>"9°");
-//Zend_Registry::set('estatusEncuesta', $estatusEncuesta);
-//============================================================================ DECORATORS
-//text form element table decorators
-$textFETDecorators = array(
-	'ViewHelper',//array('ViewHelper', array('tag' => 'td')),
-	'Errors',
-	array(array('data'=>'HtmlTag'), array('tag'=>'td', "class"=>"element")),
-	array('Label', array('tag'=>'td')),
-	array(array('row'=>'HtmlTag'), array('tag'=>'tr'))
-);
-//button form element table decorators
-$buttonFETDecorators = array(
-	'ViewHelper',//array('ViewHelper', array('tag' => 'td')),
-	array(array('data'=>'HtmlTag'), array('tag'=>'td', "class"=>"element")),
-	array('Label', array('tag'=>'td','placement'=>'prepend')),
-	array(array('row'=>'HtmlTag'), array('tag'=>'tr'))
-);
-//text subform element table decorators
-$textSETDecorators = array(
-	array('ViewHelper', array('tag' => 'td')),
-	array('Label', array('tag'=>'td')),
-	array('HtmlTag', array('tag'=>'tr'))
-);
-//subform table decorators
-$subformTDecorators = array(
-	'FormElements',
-	array('HtmlTag',array('tag'=>'tr')),
-
-);
-//form table decorators
-$formTDecorators = array(
-	'FormElements',
-	array('HtmlTag',array('tag'=>'table','class'=>'table table-striped table-condensed')),
-	'Form'
-);
-
-$modEncuesta = array(
-	'host' => '192.168.1.69',
-	'username' => 'dospesos_general',
-	'password' => 'Sgeneral2016/+',
-	'dbname' => 'dospesos_mod_encuesta',
-);
-
-//============================================================================ CONSTANTES GENERALES
-Zend_Registry::set('tipo', $tipo);
-Zend_Registry::set('tUsuario', $tUsuario);
-Zend_Registry::set('padre', $padre);
-Zend_Registry::set('estatus', $estatus);
-Zend_Registry::set('tipoEmpresa', $tipoEmpresa);
-Zend_Registry::set('tipoTelefono', $tipoTelefono);
-Zend_Registry::set('tipoSucursal', $tipoSucursal);
-Zend_Registry::set('tipoBanco', $tipoBanco);
-Zend_Registry::set('gradosEscolares', $gradosEscolares);
-Zend_Registry::set('formaPago', $formaPago);
-Zend_Registry::set('conceptoPago', $conceptoPago);
-Zend_Registry::set('tiposValores', $tiposValores);
-//Zend_Registry::set('dbconfigmodencuesta', $modEncuesta);
-
-$application->bootstrap()
-            ->run();
- * /
- 
- */
-  //============================================================== >>> Arrays de conexion a bd: usados para las clases de Zend_Db
-
+/*
+//============================================================== >>> Arrays de conexion a bd: usados para las clases de Zend_Db
  $connlocal = array(
-
 		'host' => 'localhost',
-
 		'username' => 'zazil',
-
 		'password' => 'admin',
-
 		'dbname' => 'GeneralE',
-
 	);
 
  $connlocaldos = array(
-
 		'host' => 'localhost',
-
 		'username' => 'zazil',
-
 		'password' => 'admin',
-
 		'dbname' => 'GeneralDos',
-
 	);
 
 $connserver = array(
-
 		'host' => '192.168.1.5',
-
 		'username' => 'zazil',
-
 		'password' => 'admin',
-
 		//'dbname' => 'General',
-
 		//'dbname' => 'GeneralSagrado',
-
 		'dbname' => 'GeneralE',
-
 	);
 
 $connlocaltres = array(
-
 		'host' => 'localhost',
-
 		'username' => 'zazil',
-
 		'password' => 'admin',
-
 		//'dbname' => 'General',
-
 		'dbname' => 'GeneralEncuestas',
-
 	);
 
 $connnancy = array(
-
 		'host' => '192.168.1.240',
-
 		'username' => 'areli',
-
 		'password' => 'zazil',
-
 		'dbname' => 'inventario',
-
 	);
 
 $connlocalOrigen = array(
-
 		'host' => 'localhost',
-
 		'username' => 'inventario',
-
 		'password' => 'inventario',
-
 		'dbname' => 'inventario',
-
 	);
 
-
-
 Zend_Registry::set('connlocal', $connlocal);
-
 //Zend_Registry::set('connserver', $connserver);
-
 //Zend_Registry::set('connnancy', $connnancy);
-
 //Zend_Registry::set('connlocalorigen', $connlocalOrigen);
 
 //============================================================== >>> Fijamos una conexion directa a localhost
-
 //$db = Zend_Db::factory('PDO_MYSQL', $connlocalOrigen);
 
-
-
 $db = Zend_Db::factory('PDO_MYSQL', $connlocal);
-
 //$db = Zend_Db::factory('PDO_MYSQL', $connnancy);
-
 //$db = Zend_Db::factory('PDO_MYSQL', $connlocaldos);
-
 //$db = Zend_Db::factory('PDO_MYSQL', $connserver);
-
-
-
 $db->query("SET NAMES 'utf8'");
-
 $db->query("SET CHARACTER SET 'utf8'");
-
 Zend_Db_Table_Abstract::setDefaultAdapter($db);
-
-
-
+*/
 //$dbServer = Zend_Db::factory('PDO_MYSQL', $connlocalOrigen);
-
 //Zend_Registry::set('dbInventarioLocal', $dbServer);
-
-
-
 date_default_timezone_set('America/Mexico_City');
-
-setlocale(LC_MONETARY, 'es_MX.UTF-8');
+//setlocale(LC_MONETARY, 'es_MX.UTF-8');
 
 //============================================================================ CONSTANTES GENERALES
-
 //$estatusEncuesta = array('0' => 'CREADO', '1' => 'PUBLICADO', '2' => 'ACTIVO', '3' => 'FINALIZADO');
-
 $tipo = array('AB' => 'ABIERTAS', 'SS' => 'SIMPLE SELECCION', 'MS' => 'MULTIPLE SELECCION');
-
 //$tipoInventario = array('1'=>'PEPS');
-
 $formaPago = array('CH'=>'CHEQUE','DE'=>'DEPOSITO','EF'=>'EFECTIVO','SP'=>'SPEI');
-
 $padre = array('G' => 'GRUPO', 'S' => 'SECCION');
-
 $estatus = array('A' => 'ACTIVO', 'C' => 'CANCELADO');
-
 $tUsuario = array('AL' => 'Alumna', 'DO' => 'Docente', 'MA' => 'Mantenimiento', 'LI' => 'Limpieza', 'SI' => 'Sistemas','AD' => 'Administrativo');
-
 $tipoEmpresa = array("EM"=>"Empresa","CL"=>"Cliente","PR"=>"Proveedor");
-
 $tipoBanco = array("CA" => "Caja","IN" => "Inversiones","OP" => "Operacion");
-
 $tipoTelefono = array("OF"=>"Oficina","CL"=>"Celular");
-
 $tipoSucursal = array("SE"=>"Sucursal Empresa","SC"=>"Sucursal Cliente", "SP" => "Sucursal Proveedor");
-
 //$tipoEmail = array("OF"=>"Oficina","CS"=>"Casa","PR"=>"Proveedor");
-
 $tipoMantenimiento = array("MH"=>"Mantenimiento Hardware","MS"=>"Mantenimiento Software","AV"=>"Antivirus","RO"=>"Registro Observaciones");
-
 $gradosEscolares = array(1=>"1°",2=>"2°",3=>"3°",4=>"4°",5=>"5°",6=>"6°",7=>"7°",8=>"8°",9=>"9°");
-
 //Zend_Registry::set('estatusEncuesta', $estatusEncuesta);
-
+/*
 //============================================================================ DECORATORS
-
 //text form element table decorators
-
 $textFETDecorators = array(
-
 	'ViewHelper',//array('ViewHelper', array('tag' => 'td')),
-
 	'Errors',
-
 	array(array('data'=>'HtmlTag'), array('tag'=>'td', "class"=>"element")),
-
 	array('Label', array('tag'=>'td')),
-
 	array(array('row'=>'HtmlTag'), array('tag'=>'tr'))
-
 );
 
 //button form element table decorators
-
 $buttonFETDecorators = array(
-
 	'ViewHelper',//array('ViewHelper', array('tag' => 'td')),
-
 	array(array('data'=>'HtmlTag'), array('tag'=>'td', "class"=>"element")),
-
 	array('Label', array('tag'=>'td','placement'=>'prepend')),
-
 	array(array('row'=>'HtmlTag'), array('tag'=>'tr'))
-
 );
 
 //text subform element table decorators
-
 $textSETDecorators = array(
-
 	array('ViewHelper', array('tag' => 'td')),
-
 	array('Label', array('tag'=>'td')),
-
 	array('HtmlTag', array('tag'=>'tr'))
-
 );
-
 //subform table decorators
-
 $subformTDecorators = array(
-
 	'FormElements',
-
 	array('HtmlTag',array('tag'=>'tr')),
-
-
-
 );
-
 //form table decorators
-
 $formTDecorators = array(
-
 	'FormElements',
-
 	array('HtmlTag',array('tag'=>'table','class'=>'table table-striped table-condensed')),
-
 	'Form'
-
 );
-
+*/
 //============================================================================ CONSTANTES GENERALES
-
 Zend_Registry::set('tipo', $tipo);
-
 //Zend_Registry::set('tipoInventario',$tipoInventario);
-
 Zend_Registry::set('tUsuario', $tUsuario);
-
 Zend_Registry::set('padre', $padre);
-
 Zend_Registry::set('estatus', $estatus);
-
 Zend_Registry::set('tipoEmpresa', $tipoEmpresa);
-
 Zend_Registry::set('tipoTelefono', $tipoTelefono);
-
 Zend_Registry::set('tipoSucursal', $tipoSucursal);
-
 Zend_Registry::set('tipoBanco', $tipoBanco);
-
 Zend_Registry::set('gradosEscolares', $gradosEscolares);
-
 Zend_Registry::set('formaPago', $formaPago);
 
+$application->bootstrap()->run();
 
-
-$application->bootstrap()
-
-            ->run();
