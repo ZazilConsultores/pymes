ca2c189eadaa70ef830ee740b976347e16ce522c
Areli <areli.morales@zazil.net>
2017-06-06 19:15:06 -0500

    AreliPc 06 de Junio 2017

--- library/Inventario/DAO/Productoterminado.php
+++ library/Inventario/DAO/Productoterminado.php
@@ -4,13 +4,15 @@ class Inventario_DAO_Productoterminado implements Inventario_Interfaces_IProduct
 	private $tablaProductoCompuesto;
 	private $tablaProducto;
 	private $tablaMultiplos;
-	
 	private $tablaInventario;
+	
 	
 	public function __construct()
 	{
-		//$this->tablaProducto = new Inventario_Model_DbTable_Producto;
-		//$this->tablaInventario = new Inventario_Model_DbTable_Inventario;
+		$dbAdapter = Zend_Registry::get('dbmodgeneral');
+		$this->tablaProducto = new Inventario_Model_DbTable_Producto(array('db'=>$dbAdapter));
+		$this->tablaMultiplos = new Inventario_Model_DbTable_Multiplos(array('db'=>$dbAdapter));
+		$this->tablaInventario = new Inventario_Model_DbTable_Inventario(array('db'=>$dbAdapter));
 		
 	}
 	public function obtenerProducto()
@@ -51,14 +53,37 @@ class Inventario_DAO_Productoterminado implements Inventario_Interfaces_IProduct
 		$dbAdapter = Zend_Registry::get('dbmodgeneral');
 		//$dbAdapter->beginTransaction();
 		
-		//$productoTer = $datos;
-		try{
+		//Realiza costoUnitatio por producto
+		$tablaMuliplos = $this->tablaMultiplos;
+		$select = $tablaMuliplos->select()->from($tablaMuliplos)->where("idProducto = ?",$datos[0]['productoEnlazado'])->where("idUnidad=?",$datos[0]['presentacion']);
+		$rowMultiplo = $tablaMuliplos->fetchRow($select);
+		//print_r("$select");
+		if(!is_null($rowMultiplo)){
+			$tablaInventario = $this->tablaInventario;
+			$select = $tablaInventario->select()->from($tablaInventario)->where("idProducto = ?",$datos[0]['productoEnlazado']);
+			$rowInventario = $tablaInventario->fetchRow($select);
+			//print_r("$select");
+			if(!is_null($rowInventario)){
+				//print_r("Existe producto en inventario");
+				$costoUnitario = $rowInventario->costoUnitario * $rowMultiplo->cantidad;
+				print_r($costoUnitario);
+			}else{
+				echo "El producto no esta en inventario";
+			}
+		}
+		$tablaProductoCom = $this->tablaProductoCompuesto;
+		$select = $tablaProductoCom->select()->from($tablaProductoCom)->where("idProducto = ?",$datos[0]['idProducto'])->where("productoEnlazado = ?",$datos[0]['productoEnlazado']);
+		$rowInventario = $tablaInventario->fetchRow($select);
+		print_r("$select");
+		if(is_null($rowInventario)){
+			try{
 			$mProductoTer = array(
 				'idProducto' =>$datos[0]['idProducto'],
 				'productoEnlazado' => $datos[0]['productoEnlazado'],
 				'cantidad' => $datos[0]['cantidad'],
 				'descripcion' => $datos[0]['descripcion'],
 				'presentacion' => $datos[0]['presentacion'],
+				'costoUnitario' => $costoUnitario
 				
 			);
 			$dbAdapter->insert("ProductoCompuesto", $mProductoTer);
@@ -67,5 +92,10 @@ class Inventario_DAO_Productoterminado implements Inventario_Interfaces_IProduct
 			print_r($ex->getMessage());
 			
 		}
+			
+		}else{
+			echo "El producto " .$datos[0]['productoEnlazado'] ."ya existe" ;
+		} 
+		
 	}
 }
